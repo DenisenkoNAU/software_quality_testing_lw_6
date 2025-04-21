81e231e19f408a3bfd273c80ffbbe630db677e0e
denisenko <denisenko@brights.io>
2025-04-22 00:43:00 +0300

    init linter

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    node: true,
+    jest: true
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single']
+  }
+};
